ed30d56894ef14c58b84b44ae3c0e43501a65fde
“ajiangsfu” <aijiang@bccrc.ca>
2019-10-08 11:33:28 -0700

    add top80 COO

---
+++ R/top80COO.R
@@ -0,0 +1,9 @@
+#' top 80 COO genes and their weights
+#'
+#' A dataset containing the top 80 COO ABC vs GCB genes and their weights
+#'
+#' @format A vector of weights for the DHITsig genes
+#' \describe{
+#'   \item{top80COO}{weights with top 80 COO gene names}
+#' }
+#' @source \{recently selected based on Lymph2CX COO calls in BCCA DLBCL cohort}
---
+++ R/top80COO_ENSG.R
@@ -0,0 +1,9 @@
+#' top 80 COO genes in ENSG format and their weights
+#'
+#' A dataset containing the top 80 COO ABC vs GCB genes in their ENSG foramt and their weights
+#'
+#' @format A vector of weights for the DHITsig genes
+#' \describe{
+#'   \item{top80COO}{weights with top 80 COO genes in ENSG format}
+#' }
+#' @source \{recently selected based on Lymph2CX COO calls in BCCA DLBCL cohort}
